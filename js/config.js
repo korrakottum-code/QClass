ebdc9aa0a93f3bf4fd5df92b0d638c3b2bd71e66
korrakot surapinit <korrakotsurapinit@Mac-mini-khxng-korrakot.local>
2026-02-08 02:47:03 +0700

    Update API URL to production

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-export const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbxfu59Q25jwh2sd0gtQIZXhwKjie8-hRGDMJeyrC6yEOK8Yx7THmAKRWwgulfwRclhrzA/exec';
+export const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbwXAl_W24mk92M6ftATQJzecSyrIxqDK2xqN4CncuHqZO11_xzMAgADEYIXG3tI-7xvcg/exec';

9ada178b15b11a30f257680fae1059c3e4915b91
Korrakottum <korrakottum@MacBook-Air-khxng-Korrakottum.local>
2026-02-16 01:06:02 +0700

    chore: update default API URL to new deployment

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-export const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycby9CgWuYxaWJWUTAM0agTBhkjBWVUUh8YPQjCBEmhusk62di5FcN8m8jNRsd660RyDwRQ/exec';
+export const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbynPxhCofI9xyOVc3EoYAYTYtqLF1NU9pDRb_9Kb75SFTRfK7S-ec6Jg3ROxnd9vbPsTA/exec';
